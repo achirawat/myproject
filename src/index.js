bf9f0d9805b22a74e3e9f4fdbaaf1ebd07a2fae9
Achirawat Juasawad <achirawat@angstromsolutions.net>
2018-07-13 14:23:28 +0700

    .

--- src/index.js
+++ src/index.js
@@ -8,5 +8,6 @@ import { BrowserRouter } from 'react-router-dom';
 ReactDOM.render(
     <BrowserRouter>
         <App />
-    </BrowserRouter>, document.getElementById('root'));
+    </BrowserRouter>, 
+    document.getElementById('root'));
 registerServiceWorker();
